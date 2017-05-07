2b139e01d34cfb646712e3f8b3f2c5353d9ff6bb
zhiaifeihuan <zhiaifeihuan@qq.com>
2017-05-08 01:34:06 +0800

    部署hexo博客源码

---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+/*
+console.log("start adding tags");
+hexo.extend.generator.register('tags', function(locals){
+  var newlocal=locals;
+  newlocal.posts=locals.posts.sort('-date');
+  return {
+    path: 'tags/index.html',
+    data: newlocal,
+    layout: ['index']
+  };
+});
+console.log("done!");
+*/
---
+++ source/_posts/test.md
@@ -0,0 +1,8 @@
+---
+title: test
+date: 2017-05-06 21:46:10
+comments: true
+tags:
+---
+# 大家好
+std::cout<<"hello world!";
